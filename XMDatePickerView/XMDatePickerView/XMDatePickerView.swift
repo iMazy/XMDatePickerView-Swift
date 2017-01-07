5809683a4cc9db106ebe9ce5ee4e6b6db9d71196
Mazy <twt_ios@163.com>
2017-01-07 12:43:48 +0800

    添加XMDatePickerView工具类,并关联XIB

---
+++ XMDatePickerView/XMDatePickerView/XMDatePickerView.swift
@@ -0,0 +1,21 @@
+//
+//  XMDatePickerView.swift
+//  XMDatePickerView
+//
+//  Created by TwtMac on 17/1/7.
+//  Copyright © 2017年 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class XMDatePickerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
