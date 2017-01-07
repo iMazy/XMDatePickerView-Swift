f2d7ddc320204a6f27666ac9a36dd4f7260c745f
Mazy <twt_ios@163.com>
2017-01-07 12:57:37 +0800

    将XIB文件中的对象与视图进行关联

--- XMDatePickerView/XMDatePickerView/XMDatePickerView.swift
+++ XMDatePickerView/XMDatePickerView/XMDatePickerView.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class XMDatePickerView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    /// 背景视图-->按钮,自带点击事件
+    @IBOutlet weak var backgroundButton: UIButton!
+    /// 时间选择器的父视图
+    @IBOutlet weak var parentView: UIView!
+    /// 时间选择器
+    @IBOutlet weak var datePicker: UIDatePicker!
+    /// 取消按钮
+    @IBOutlet weak var cancelButton: UIButton!
+    /// 确定按钮
+    @IBOutlet weak var sureButton: UIButton!
+    
+    
+    
+    /// 背景按钮点击事件
+    @IBAction func backgroundBtnClick() {
+        
     }
-    */
-
+    
+    /// 取消按钮点击事件
+    @IBAction func cancelBtnClick() {
+    }
+    
+    /// 确定按钮点击事件
+    @IBAction func sureBtnClick() {
+    }
 }
